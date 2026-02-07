1119eeac6a5eaa2a1106e52fd4f2066f026219ba
Masura <masurabegum62@gmail.com>
2026-02-07 09:53:25 +0530

    controller and service adding

--- src/main/java/com/projects/urlShortener/controller/ShortUrlController.java
+++ src/main/java/com/projects/urlShortener/controller/ShortUrlController.java
@@ -1,14 +1,37 @@
 package com.projects.urlShortener.controller;
 
+import com.projects.urlShortener.dto.ShortenRequest;
+import com.projects.urlShortener.dto.ShortenResponse;
+import com.projects.urlShortener.service.ShortUrlService;
+import jakarta.validation.Valid;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/shorten")
 public class ShortUrlController {
-    @GetMapping
-    public String test() {
-        return "URL Shortener Service is up and running!";
+
+    private final ShortUrlService service;
+
+    public ShortUrlController(ShortUrlService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/api/shorten")
+    public ResponseEntity<ShortenResponse> shortenUrl(@Valid @RequestBody ShortenRequest request) {
+        return ResponseEntity.ok(service.shortenUrl(request));
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> getOriginalUrl(@PathVariable String shortCode) {
+        String originalUrl = service.getOriginalUrl(shortCode);
+
+        return ResponseEntity.status(302).location(URI.create(originalUrl)).build();
     }
 }
--- src/main/java/com/projects/urlShortener/service/ShortUrlService.java
+++ src/main/java/com/projects/urlShortener/service/ShortUrlService.java
@@ -1,4 +1,47 @@
 package com.projects.urlShortener.service;
 
+import com.projects.urlShortener.dto.ShortenRequest;
+import com.projects.urlShortener.dto.ShortenResponse;
+import com.projects.urlShortener.model.ShortUrl;
+import com.projects.urlShortener.repository.ShortUrlRepository;
+import com.projects.urlShortener.util.Base62Encoder;
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ShortUrlService {
+
+    private final ShortUrlRepository repository;
+    private static final String BASE_URL = "http://localhost:8080/";
+
+    public ShortUrlService(ShortUrlRepository repository) {
+        this.repository = repository;
+    }
+
+    public ShortenResponse shortenUrl(ShortenRequest request) {
+        ShortUrl entity =
+                new ShortUrl.Builder()
+                        .originalUrl(request.getOriginalUrl())
+                        .createdAt(LocalDateTime.now())
+                        .build();
+
+        entity = repository.save(entity);
+
+        String shortCode = Base62Encoder.encode(entity.getId());
+        entity.setShortCode(shortCode);
+        repository.save(entity);
+
+        return new ShortenResponse.Builder()
+                .shortUrl(BASE_URL + shortCode)
+                .originalUrl(request.getOriginalUrl())
+                .createdAt(entity.getCreatedAt())
+                .build();
+    }
+
+    public String getOriginalUrl(String ShortCode) {
+        return repository
+                .findByShortCode(ShortCode)
+                .map(ShortUrl::getOriginalUrl)
+                .orElseThrow(() -> new RuntimeException("Short URL not found"));
+    }
 }
